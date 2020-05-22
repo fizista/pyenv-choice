8c326c63342b2c46d405de5dd99a6094c25349fa
Wojciech Banaś <fizista@gmail.com>
2020-05-22 11:56:01 +0200

    New version
    It contains:
    * selection of your own local version of the command
    * choose your own global version of the command
    * automatic selection of the command from the last installed environment

--- README.md
+++ README.md
@@ -1,33 +1,74 @@
-# pyenv-implict
-> Allow pyenv to guess the python version from the program name.
+# pyenv-choice
+
+It allows you to choose which version of Python you will use for a given program name, 
+if there are multiple versions of this program.
 
 ## Install
 
-Installing **pyenv-implict** as a pyenv plugin will give you access to the provided behavior.
+Installing **pyenv-choice** as a pyenv plugin will give you access to the provided behavior.
 
 ```sh
-$ git clone git://github.com/concordusapps/pyenv-implict.git ~/.pyenv/plugins/pyenv-implict
+$ git clone https://github.com/fizista/pyenv-choice.git ~/.pyenv/plugins/pyenv-choice
 ```
 
 ## Usage
 
+Setting the chosen python version:
 ```sh
-$ pyenv install 2.7.5
-$ pyenv install 3.3.2
+$ pyenv install 3.6.5
+$ pyenv install 3.8.1
+$ pyenv version
+system (set by ...)
+
+$ python3 --version 2>/dev/null
+Python 3.8.1
+
+$ pyenv cmd-local python3 3.6.5
+
+$ python3 --version 2>/dev/null
+Python 3.6.5
+```
+
+Setting the selected python version for the selected command:
+```sh
+$ pyenv install 3.6.5
+$ pyenv install 3.8.1
+$ pyenv shell 3.6.5
+$ pip install pipenv
+$ pyenv shell 3.8.1
+$ pip install pipenv
+$ pyenv shell --unset
 
 $ pyenv version
 system (set by ...)
 
-$ python2.7 --version
-Python 2.7.5
+$ pipenv --version
+############################### pyenv-choice #################################
+Found multiple pipenv versions. The command was launched from a manually
+selected version of the python '3.8.1'.
+You can change it with the commands: 
+  > pyenv cmd-global pipenv <version>
+  > pyenv cmd-local pipenv <version>
+##############################################################################
 
-$ pyenv shell 3.3.2
-$ pyenv version
-3.3.2 (set by PYENV_VERSION environment variable)
+pipenv, version 2018.11.26
 
-$ python2 --version
-Python 2.7.5
+$ pyenv cmd-local pipenv 3.6.5
+$ pipenv --version
+############################### pyenv-choice #################################
+Found multiple pipenv versions. The command was launched from a manually
+selected version of the python '3.6.5'.
+You can change it with the commands: 
+  > pyenv cmd-global pipenv <version>
+  > pyenv cmd-local pipenv <version>
+##############################################################################
+
+pipenv, version 2018.11.26
+
 ```
+
+
+
 
 ## License
 Unless otherwise noted, all files contained within this project are liensed under the MIT opensource license. See the included file LICENSE or visit [opensource.org][] for more information.
--- etc/pyenv.d/which/whence.bash
+++ etc/pyenv.d/which/whence.bash
@@ -2,8 +2,44 @@ if [ -n "$PYENV_COMMAND" ] && [ ! -x "$PYENV_COMMAND_PATH" ]; then
   versions=($(pyenv-whence "${PYENV_COMMAND}" 2>/dev/null || true))
   if [ -n "${versions}" ]; then
     if [ "${#versions[@]}" -gt 1 ]; then
-      echo "pyenv-implicit: found multiple ${PYENV_COMMAND} in pyenv. Use version ${versions[0]}." 1>&2
+      main_version=$(pyenv-cmd-local "${PYENV_COMMAND}" 2>/dev/null || true)
+      if [ -n "$main_version" ]; then
+        echo -e "############################### \e[1mpyenv-choice\e[0m #################################" 1>&2
+        echo -e "Found multiple \e[1m${PYENV_COMMAND}\e[0m versions. The command was launched from a manually" 1>&2
+        echo -e "selected version of the python \e[1m'${main_version}'\e[0m." 1>&2
+        echo "You can change it with the commands: " 1>&2
+        echo "  > pyenv cmd-global ${PYENV_COMMAND} <version>" 1>&2
+        echo "  > pyenv cmd-local ${PYENV_COMMAND} <version>" 1>&2
+        echo "##############################################################################" 1>&2
+        echo "" 1>&2
+        PYENV_COMMAND_PATH="${PYENV_ROOT}/versions/${main_version}/bin/${PYENV_COMMAND}"
+        if [ ! -f "$PYENV_COMMAND_PATH" ]; then
+          echo "The '${PYENV_COMMAND}' command does not exist for the '${main_version}' version." 1>&2
+          exit 1
+        fi
+      else
+        function join_by { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
+        available_versions=$(join_by '$|^' $(pyenv whence "${PYENV_COMMAND}" 2>/dev/null | grep -v '/'))
+        if [ -z "$available_versions" ]; then
+          echo "The '${PYENV_COMMAND}' command does not exist in any installed python version." 1>&2
+          exit 1
+        fi
+        last_version=$(ls -t ${PYENV_ROOT}/versions/ | awk "\$1 ~ /^${available_versions}/" | head -n1)
+        PYENV_COMMAND_PATH="${PYENV_ROOT}/versions/${last_version}/bin/${PYENV_COMMAND}"
+        if [ ! -f "$PYENV_COMMAND_PATH" ]; then
+          echo "The '${PYENV_COMMAND}' command does not exist for the '${main_version}' version." 1>&2
+          exit 1
+        fi
+        echo -e "############################### \e[1mpyenv-choice\e[0m #################################" 1>&2
+        echo -e "Found multiple \e[1m${PYENV_COMMAND}\e[0m versions. The command was launched from" 1>&2
+        echo -e "the last installed version of the python \e[1m'${last_version}'\e[0m." 1>&2
+        echo "You can change it with the commands: " 1>&2
+        echo "  > pyenv cmd-global ${PYENV_COMMAND} <version>" 1>&2
+        echo "  > pyenv cmd-local ${PYENV_COMMAND} <version>" 1>&2
+        echo "##############################################################################" 1>&2
+        echo "" 1>&2
+      fi
     fi
-    PYENV_COMMAND_PATH="${PYENV_ROOT}/versions/${versions[0]}/bin/${PYENV_COMMAND}"
   fi
 fi
+
